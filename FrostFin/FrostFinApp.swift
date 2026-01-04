7f25275a085fd5e018e8ec6884d8a9884b6cc418
ludik <bakhanetssimon@gmail.com>
2026-01-04 16:47:04 +0700

    Initial Commit

---
+++ FrostFin/FrostFinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostFinApp.swift
+//  FrostFin
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FrostFinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrostFinTests/FrostFinTests.swift
@@ -0,0 +1,16 @@
+//
+//  FrostFinTests.swift
+//  FrostFinTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct FrostFinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
